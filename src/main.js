f992c60d76b3b616ebada02c7e06b8527c858e71
Bence0107 <zsupposkovacsbence@gmail.com>
2026-09-24 10:00:20 +0200

    active button design

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
-import { programs } from './data/data'
-import './style.css'
-import { renderCateg, renderPrograms } from './ui/render'
+import { programs } from './data/data';
+import './style.css';
+import { renderCateg, renderFooter, renderPrograms } from './ui/render';
+import { createIcons, icons } from 'lucide';
+createIcons({ icons });
 
-renderPrograms(programs)
-renderCateg(programs)
+renderPrograms(programs);
+renderCateg(programs);
+renderFooter(programs);
 
 window.onCategClick = function onCategClick(e) {
-  console.log(e.target.textContent);
+  if (e.target.tagName != "BUTTON") return;
+  document.querySelectorAll("button").forEach(button => button.classList.remove("activeBtn"));
+  e.target.classList.add("activeBtn");
   const selectedCateg = e.target.textContent;
   const filteredPrograms = programs.filter(obj => obj.category == selectedCateg);
   selectedCateg == "összes" ? renderPrograms(programs) : renderPrograms(filteredPrograms);
+  document.querySelector(".btn").classList.remove("activeBtn");
+}
+
+window.availablePrograms = function availablePrograms() {
+  const filteredPrograms = programs.filter(obj => obj.capacity > obj.participants);
+  renderPrograms(filteredPrograms);
+  document.querySelectorAll("button").forEach(button => button.classList.remove("activeBtn"));
+  document.querySelector(".btn").classList.add("activeBtn");
 }
--- src/ui/render.js
+++ src/ui/render.js
@@ -4,9 +4,9 @@ export function renderPrograms(arr) {
         <div class="card">
             <h3>${obj.title}</h3>
             <h4>Kategória: ${obj.category}</h4>
-            <p>Ár: ${obj.price}</p>
-            <p>Résztvevők: ${obj.participants}</p>
-            <p>Helyek: ${obj.capacity}</p>
+            <p>Ár: ${obj.price} Ft</p>
+            <p>Maximális létszám: ${obj.capacity}</p>
+            <p>Szabad helyek: ${obj.capacity - obj.participants}</p>
             <p>${obj.indoor ? "beltéri" : "kültéri"}</p>
         </div>
         `
@@ -16,14 +16,23 @@ export function renderPrograms(arr) {
 
 export function renderCateg(arr) {
     let categories = arr.map(obj => obj.category);
-    console.log(categories);
     categories = [...new Set(categories)];
-    console.log(categories);
-    categories = [...categories, "összes"]
     const btnStr = categories.map(ctg =>
         `
         <button>${ctg}</button>
         `
     ).join("");
-    document.querySelector("header").innerHTML = btnStr;
+    document.querySelector("header").innerHTML = "<button class='activeBtn'>összes</button>" + btnStr;
+}
+
+export function renderFooter(arr) {
+    const ossz_resztv = arr.reduce((acc, obj) => acc + obj.participants, 0);
+    const atl_resztv_dij = arr.reduce((acc, obj) => acc + obj.price, 0) / arr.length;
+    const jelen_bevetel = arr.reduce((acc, obj) => acc + obj.price * obj.participants, 0);
+    const belteri_sum = arr.reduce((acc, obj) => obj.indoor ? acc + 1 : acc, 0);
+    const kulteri_sum = arr.reduce((acc, obj) => obj.indoor ? acc : acc + 1, 0);
+    document.querySelector("footer").innerHTML =
+        `
+    <p>Összes résztvevő: ${ossz_resztv}, Átlagos részvételi díj: ${atl_resztv_dij} Ft, Jelenlegi bevétel: ${jelen_bevetel} Ft, Beltéri programok: ${belteri_sum}, Kültéri programok: ${kulteri_sum}</p>
+    `;
 }
